e476ee4c6749ba524e54c97832acc2a6c041d51e
Brandon <b.shega@gmail.com>
2014-12-15 22:12:50 -0500

    Initial commit

---
+++ Glamify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Glamify
+//
+//  Created by Brandon Shega on 12/15/14.
+//  Copyright (c) 2014 Brandon Shega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
